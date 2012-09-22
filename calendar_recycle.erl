0b99949a8a8e096847f89504f0e003374bc75c41
Ryan Flynn <parseerror@gmail.com>
2012-09-22 15:41:55 -0700

    slightly more readable

--- calendar_recycle.erl
+++ calendar_recycle.erl
@@ -1,13 +1,16 @@
 #!/usr/bin/env escript
 %% -*- erlang -*-
 
+-author("Ryan Flynn parseerror@gmail.com www.parseerror.com github.com/rflynn").
+
 % figure out which year's calendars are identical in structure so we can recycle them
 % inspiration: http://garyc.me/calendars/
 % NOTE: shortcoming: lunar cycle
 
 main(_) ->
     AllYears = [
-        {{calendar:day_of_the_week(Year,1,1), calendar:is_leap_year(Year)},Year}
+        {{calendar:day_of_the_week(Year,1,1),
+          calendar:is_leap_year(Year)},Year}
             || Year <- lists:seq(1900,2100)],
     MergedByKey =
         lists:foldl(
