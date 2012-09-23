ef4f73395ca6d0509cb8a3d7f521958f6235d20f
Ryan Flynn <parseerror@gmail.com>
2012-09-22 17:30:58 -0700

    golf calendar down into 3 lines

--- calendar_recycle.erl
+++ calendar_recycle.erl
@@ -8,13 +8,7 @@
 % NOTE: shortcoming: lunar cycle
 
 main(_) ->
-    AllYears = [
-        {{calendar:day_of_the_week(Year,1,1),
-          calendar:is_leap_year(Year)},Year}
-            || Year <- lists:seq(1900,2100)],
-    MergedByKey =
-        lists:foldl(
-            fun({K,V},O) -> orddict:append(K,V,O) end,
-            orddict:new(), AllYears),
-    io:format("~p~n", [MergedByKey]).
+    io:format("~p~n",
+        [lists:foldl(fun({K,V},O) -> orddict:append(K,V,O) end, orddict:new(),
+            [{{calendar:day_of_the_week(Year,1,1), calendar:is_leap_year(Year)}, Year} || Year <- lists:seq(1900,2100)])]).
 
